573364b0d14c3c2dfe21c236f2e0fd3e5e3d103e
shriram.sm <shriram.sm@operative.com>
2022-07-24 19:50:13 +0530

    Added Swagger Documentation for Add Expense Record

--- apis/apis/src/main/java/com/expensetracker/apis/ApisApplication.java
+++ apis/apis/src/main/java/com/expensetracker/apis/ApisApplication.java
@@ -2,12 +2,33 @@ package com.expensetracker.apis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ApisApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApisApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ApisApplication.class, args);
+  }
+
+  @Bean
+  public Docket api() {
+    // return prepared docket instance
+    return new Docket(DocumentationType.SWAGGER_2).select()
+        .apis(RequestHandlerSelectors.basePackage("com.expensetracker")).paths(PathSelectors.any())
+        .build().apiInfo(apiDetails());
+  }
+
+  private ApiInfo apiDetails() {
+    return new ApiInfo("Expense Tracker Application", "Backend APIs for Application", "Version 1",
+        "Free to use", "Team Sankalp", null, null);
+  }
 
 }
